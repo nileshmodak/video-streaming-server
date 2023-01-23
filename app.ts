69914c6afed1f8010cd5ce9288668034b32c72ce
Nilesh Modak <nilesh2git@gmail.com>
2023-01-23 12:44:29 +0530

    Added cors

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ app.get('/video/:id/poster', async(req, res) => {
 });
 
 app.get('/video/:id', (req, res) => {
-    /*const fileName = getFileName(videoList, req.params.id);
+    const fileName = getFileName(videoList, req.params.id);
     let range = req.headers.range;
 
     if(!range) {
@@ -54,35 +54,7 @@ app.get('/video/:id', (req, res) => {
     res.writeHead(206, headers);
 
     const videoStream = fs.createReadStream(path, { start, end });
-    videoStream.pipe(res);*/
-
-    const videoPath = `${VIDEO_DIR}/${getFileName(videoList, req.params.id)}`;
-    const videoStat = fs.statSync(videoPath);
-    const fileSize = videoStat.size;
-    const videoRange = req.headers.range;
-    if (videoRange) {
-        const parts = videoRange.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1]
-            ? parseInt(parts[1], 10)
-            : fileSize-1;
-        const chunksize = (end-start) + 1;
-        const file = fs.createReadStream(videoPath, {start, end});
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(videoPath).pipe(res);
+    videoStream.pipe(res);
     }
 });
 
